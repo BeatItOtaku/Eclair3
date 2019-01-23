23f103c16a0987f1de8c96babb0cb052b0cf2685
wararyo <wararyo@gmail.com>
2019-01-24 05:32:32 +0900

    暗号化方式をCBCに変更するも進捗ダメです

--- Source/Eclair/ResultEncryptor.cpp
+++ Source/Eclair/ResultEncryptor.cpp
@@ -16,6 +16,7 @@ FString UResultEncryptor::EncryptResult(int time, int item, FDateTime timestamp)
 	auto Writer = TJsonWriterFactory<>::Create(&Content);
 	FJsonSerializer::Serialize(JsonObject, Writer);
 
+	//return Encrypt("ThisIsCharacters");
 	return Encrypt("{\"time\":86,\"item\":12,\"timestamp\":1548231246}");
 	//return Encrypt(Content);
 }
@@ -31,32 +32,42 @@ FString UResultEncryptor::Encrypt(FString str)
 	//data size need to be aligned with block size
 
 	Size = str.Len();
-	Size = Size + (FAES::AESBlockSize - (Size % FAES::AESBlockSize));
+	//Size++;
+	//Size = Size + (AES_BLOCK_SIZE - (Size % AES_BLOCK_SIZE));
 
 	// ƒL[‚ð¶¬
 	FString Key = "hogehogefooofooohogehogefooofooo";                            // Choose a key then...
 	TCHAR *KeyTChar = Key.GetCharArray().GetData();            // ...turn key string...
 	ANSICHAR *KeyAnsi = (ANSICHAR*)TCHAR_TO_ANSI(KeyTChar);
+	unsigned char *KeyChar = new unsigned char[AES_KEYLENGTH];
+	memset(KeyChar, 0x00, AES_KEYLENGTH);
+	strcpy((char*)KeyChar, (char*)TCHAR_TO_UTF8(KeyTChar));
 
 	Blob = new uint8[Size]; //So once we calculated size we allocating space in memory 
 								//which we use for encryption
 
 	TCHAR *c = str.GetCharArray().GetData();
-	unsigned char *strByte = (unsigned char*)TCHAR_TO_UTF8(c);
+	unsigned char *in = new unsigned char[Size];
+	memset(in, 0x00, Size);
+	strcpy((char*)in,(char*)TCHAR_TO_UTF8(c));
 
 	int utf8Size = 256; //strlen((char*)strByte);
-	unsigned char* out = new unsigned char[utf8Size];
+	unsigned char* out = new unsigned char[Size];
+
+	/* init vector */
+	unsigned char iv[AES_BLOCK_SIZE];
+	memset(iv, 0x00, AES_BLOCK_SIZE);
 
 	UE_LOG(LogTemp, Verbose, TEXT("Size:%d, uft8Size:%d"), Size, utf8Size);
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Size:%d, uft8Size:%d, strByte:%s"), Size, utf8Size, UTF8_TO_TCHAR(strByte)));
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("Size:%d, uft8Size:%d, strByte:%s"), Size, utf8Size, UTF8_TO_TCHAR(in)));
 
 								//We filling allocated space with string to process
-	if (strByte != nullptr) {
+	if (in != nullptr) {
 		//FAES::EncryptData((unsigned char*)c, Size, KeyAnsi); //We encrypt the data, don't know how you want to input key
 		AES_KEY aesKey;
-		AES_set_encrypt_key((unsigned char*)KeyAnsi, AES_KEYLENGTH, &aesKey);
-		AES_ecb_encrypt((unsigned char*)strByte, out, &aesKey, AES_ENCRYPT);
-		str = FBase64::Encode(out, utf8Size);
+		AES_set_encrypt_key((unsigned char*)KeyChar, AES_KEYLENGTH, &aesKey);
+		AES_cbc_encrypt((unsigned char*)in, out,Size, &aesKey,iv, AES_ENCRYPT);
+		str = FBase64::Encode(out, Size);
 		delete Blob; //deleting allocation for safety
 		return str; //and return it
 	}
